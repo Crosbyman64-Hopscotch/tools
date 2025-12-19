696c4b54f78fd4e633dd1571bd73d04c738dfa28
Crosbyman64 <crosbyman216@gmail.com>
2025-12-18 22:25:07 -0500

    update code syntax

--- hs-project-to-script/dataToScript.js
+++ hs-project-to-script/dataToScript.js
@@ -22,13 +22,13 @@ fs.addEventListener("change",function(){
     });
     n.readAsText(this.files[0])
 });
-var regex=/^-?([0-9]*\.)?([0-9]+e[+-]?)?[0-9]+$/,result,customBlocks,params,ops=[{"id":4004,"type":"Math.random"},{"id":4006,"type":"Math.sqrt"},{"id":4007,"type":"sin"},{"id":4008,"type":"cos"},{"id":4009,"type":"round"},{"id":4010,"type":"abs"},{"id":4012,"type":"tan"},{"id":4013,"type":"arcsin"},{"id":4014,"type":"arccos"},{"id":4015,"type":"arctan"},{"id":4016,"type":"max"},{"id":4017,"type":"min"},{"id":4018,"type":"floor"},{"id":4019,"type":"ceil"},{"id":9000,"type":"charAtIndex"},{"id":9001,"type":"charsInRange"},{"id":9002,"type":"length"},{"id":9003,"type":"join"}],traitTypes=[{"id":8000,"type":"object"},{"id":8001,"type":"anyObject"},{"id":8002,"type":"edge"},{"id":8003,"type":"device"},{"id":8004,"type":"self"},{"id":8005,"type":"originalObject"}],events=[{"id":7000,"type":"gameStarts"},{"id":7021,"type":"gameIsPlaying"},{"id":7001,"type":"isTapped"},{"id":7003,"type":"isPressed"},{"id":7020,"type":"isNotPressed"},{"id":7022,"type":"touchEnds"},{"id":7023,"type":"iReceive"},{"id":7024,"type":"message.match"},{"id":7008,"type":"devicesHearsNoise"},{"id":7009,"type":"deviceIsShaken"},{"id":7011,"type":"isSwipedRight"},{"id":7012,"type":"isSwipedLeft"},{"id":7013,"type":"isSwipedUp"},{"id":7014,"type":"isSwipedDown"},{"id":7016,"type":"deviceIsTiltedRight"},{"id":7017,"type":"deviceIsTiltedLeft"},{"id":7018,"type":"deviceIsTiltedUp"},{"id":7019,"type":"deviceIsTiltedDown"},{"id":1010,"type":"flipped"},{"id":7015,"type":"isCloned"},{"id":7010,"type":"bumps"},{"id":7002,"type":"isTouching"},{"id":7025,"type":"isNotTouching"}],HSBlocks=[{"id":19,"type":"waitTilTimestamp"},{"id":23,"type":"moveForward"},{"id":24,"type":"turn"},{"id":41,"type":"setPosition"},{"id":50,"type":"flip"},{"id":27,"type":"changeX"},{"id":28,"type":"changeY"},{"id":34,"type":"setSpeed"},{"id":39,"type":"setAngle"},{"id":59,"type":"setOrigin"},{"id":70,"type":"setBackground"},{"id":54,"type":"setColor"},{"id":56,"type":"setImage"},{"id":40,"type":"setText"},{"id":51,"type":"setSize"},{"id":57,"type":"setWidthAndHeight"},{"id":48,"type":"grow"},{"id":49,"type":"shrink"},{"id":62,"type":"startSound"},{"id":52,"type":"startSoundMS"},{"id":47,"type":"setInvisibility"},{"id":43,"type":"bringToFront"},{"id":42,"type":"sendToBack"},{"id":33,"type":"changePose"},{"id":58,"type":"setZIndex"},{"id":66,"type":"setTempo"},{"id":67,"type":"setInstrument"},{"id":65,"type":"playNote"},{"id":72,"type":"alert"},{"id":64,"type":"setTextToInput"},{"id":32,"type":"setTrailColor"},{"id":31,"type":"setTrailWidth"},{"id":73,"type":"setTrailOpacity"},{"id":71,"type":"setTrailCap"},{"id":30,"type":"clear"},{"id":61,"type":"wait"},{"id":35,"type":"waitMS"},{"id":53,"type":"cloneObject"},{"id":55,"type":"destroy"},{"id":68,"type":"openProject"},{"id":125,"type":"changeScene"},{"id":126,"type":"broadcastMessage"},{"id":127,"type":"requestSeeds"},{"id":29,"type":"scale"},{"id":36,"type":"setOpacity"},{"id":22,"type":"comment"},{"id":37,"type":"penUp"},{"id":38,"type":"penDown"}],chars=[{"id":0,"type":"Monkey"},{"id":2,"type":"Octopus"},{"id":3,"type":"Gorilla"},{"id":4,"type":"Cupcake"},{"id":5,"type":"Bear"},{"id":6,"type":"Dino"},{"id":7,"type":"Frog"},{"id":8,"type":"Jody"},{"id":9,"type":"Mr. Mustache"},{"id":10,"type":"Space Pod"},{"id":11,"type":"Zombear"},{"id":12,"type":"Ghoulopus"},{"id":13,"type":"Bats"},{"id":14,"type":"Frankenrilla"},{"id":15,"type":"Witch Jody"},{"id":16,"type":"Cauldron"},{"id":17,"type":"Pumpkin"},{"id":18,"type":"Broom"},{"id":3002,"type":"Lantern"},{"id":20,"type":"Parrot"},{"id":21,"type":"Mandrill"},{"id":22,"type":"Mosquito"},{"id":23,"type":"Miss Chief"},{"id":24,"type":"Venus"},{"id":25,"type":"Jeepers"},{"id":26,"type":"Banyan"},{"id":27,"type":"Star Girl"},{"id":28,"type":"Cosmic Cody"},{"id":29,"type":"Chillanna"},{"id":30,"type":"Robo"},{"id":31,"type":"Raccoon"},{"id":32,"type":"Bird"},{"id":58,"type":"Toucan"},{"id":59,"type":"Anteater"},{"id":3001,"type":"Crocodile"},{"id":61,"type":"Sloth"},{"id":62,"type":"Iguana"},{"id":63,"type":"Hut"},{"id":64,"type":"Penguin"},{"id":65,"type":"Queen Ana"},{"id":66,"type":"Yeti"},{"id":67,"type":"Deer"},{"id":68,"type":"Elf"},{"id":69,"type":"Snow Globe"},{"id":70,"type":"Polar Bear"},{"id":71,"type":"Sleigh"},{"id":72,"type":"Mistletoe"},{"id":73,"type":"Gonzalo"},{"id":74,"type":"Snowflake"},{"id":153,"type":"Heart"},{"id":155,"type":"Arch"},{"id":156,"type":"Squiggle"},{"id":154,"type":"Star"},{"id":163,"type":"Parallelogram"},{"id":112,"type":"Donut"},{"id":160,"type":"Fan"},{"id":102,"type":"Circle"},{"id":101,"type":"Square"},{"id":150,"type":"Hexagon"},{"id":151,"type":"Triangle"},{"id":105,"type":"Right Triangle"},{"id":152,"type":"Rectangle"},{"id":157,"type":"Z"},{"id":158,"type":"T"},{"id":159,"type":"L"},{"id":116,"type":"Corner"},{"id":117,"type":"Flower"},{"id":119,"type":"Squished Box"},{"id":162,"type":"Bead"},{"id":164,"type":"Chevron"},{"id":125,"type":"X"},{"id":126,"type":"Tetris Line"},{"id":120,"type":"Rounded Right Triangle"},{"id":161,"type":"Rounded Arrow"},{"id":100,"type":"Rounded Square"},{"id":110,"type":"Tilted Rectangle"}]
+var regex=/^-?([0-9]*\.)?([0-9]+e[+-]?)?[0-9]+$/,result,customBlocks,params,ops=[{"id":4004,"type":"Math.random"},{"id":4006,"type":"Math.sqrt"},{"id":4007,"type":"sin"},{"id":4008,"type":"cos"},{"id":4009,"type":"round"},{"id":4010,"type":"abs"},{"id":4012,"type":"tan"},{"id":4013,"type":"arcsin"},{"id":4014,"type":"arccos"},{"id":4015,"type":"arctan"},{"id":4016,"type":"max"},{"id":4017,"type":"min"},{"id":4018,"type":"floor"},{"id":4019,"type":"ceil"},{"id":9000,"type":"char_at_index"},{"id":9001,"type":"chars_in_range"},{"id":9002,"type":"length"},{"id":9003,"type":"join"}],traitTypes=[{"id":8000,"type":"object"},{"id":8001,"type":"any_object"},{"id":8002,"type":"edge"},{"id":8003,"type":"device"},{"id":8004,"type":"self"},{"id":8005,"type":"original_object"}],events=[{"id":7000,"type":"game_starts"},{"id":7021,"type":"game_is_playing"},{"id":7001,"type":"is_tapped"},{"id":7003,"type":"is_pressed"},{"id":7020,"type":"is_not_pressed"},{"id":7022,"type":"touch_ends"},{"id":7023,"type":"i_get_a_message"},{"id":7024,"type":"message.match"},{"id":7008,"type":"devices_hears_noise"},{"id":7009,"type":"device_is_shaken"},{"id":7011,"type":"is_swiped_right"},{"id":7012,"type":"is_swiped_left"},{"id":7013,"type":"is_swiped_up"},{"id":7014,"type":"is_swiped_down"},{"id":7016,"type":"device_is_tilted_right"},{"id":7017,"type":"device_is_tilted_left"},{"id":7018,"type":"device_is_tilted_up"},{"id":7019,"type":"device_is_tilted_down"},{"id":1010,"type":"flipped"},{"id":7015,"type":"cloned"},{"id":7010,"type":"bumps"},{"id":7002,"type":"is_touching"},{"id":7025,"type":"is_not_touching"}],HSBlocks=[{"id":19,"type":"wait_til_timestamp"},{"id":23,"type":"move_forward"},{"id":24,"type":"turn"},{"id":41,"type":"set_position"},{"id":50,"type":"flip"},{"id":27,"type":"change_x"},{"id":28,"type":"change_y"},{"id":34,"type":"set_speed"},{"id":39,"type":"set_angle"},{"id":59,"type":"set_origin"},{"id":70,"type":"set_background"},{"id":54,"type":"set_color"},{"id":56,"type":"set_image"},{"id":40,"type":"set_text"},{"id":51,"type":"set_size"},{"id":57,"type":"set_width_and_height"},{"id":48,"type":"grow"},{"id":49,"type":"shrink"},{"id":62,"type":"start_sound"},{"id":52,"type":"start_sound_ms"},{"id":47,"type":"set_invisibility"},{"id":43,"type":"bring_to_front"},{"id":42,"type":"send_to_back"},{"id":33,"type":"change_pose"},{"id":58,"type":"set_z_index"},{"id":66,"type":"set_tempo"},{"id":67,"type":"set_instrument"},{"id":65,"type":"play_note"},{"id":72,"type":"alert"},{"id":64,"type":"set_text_to_input"},{"id":32,"type":"set_trail_color"},{"id":31,"type":"set_trail_width"},{"id":73,"type":"set_trail_opacity"},{"id":71,"type":"set_trail_cap"},{"id":30,"type":"clear"},{"id":61,"type":"wait"},{"id":35,"type":"wait_ms"},{"id":53,"type":"clone_object"},{"id":55,"type":"destroy"},{"id":68,"type":"open_project"},{"id":125,"type":"change_scene"},{"id":126,"type":"broadcast_message"},{"id":127,"type":"request_seeds"},{"id":29,"type":"scale"},{"id":36,"type":"set_opacity"},{"id":22,"type":"comment"},{"id":37,"type":"pen_up"},{"id":38,"type":"pen_down"}],chars=[{"id":0,"type":"monkey"},{"id":2,"type":"octopus"},{"id":3,"type":"gorilla"},{"id":4,"type":"cupcake"},{"id":5,"type":"bear"},{"id":6,"type":"dino"},{"id":7,"type":"frog"},{"id":8,"type":"jody"},{"id":9,"type":"mr_mustache"},{"id":10,"type":"space_pod"},{"id":11,"type":"zombear"},{"id":12,"type":"ghoulopus"},{"id":13,"type":"bats"},{"id":14,"type":"frankenrilla"},{"id":15,"type":"witch_jody"},{"id":16,"type":"cauldron"},{"id":17,"type":"pumpkin"},{"id":18,"type":"broom"},{"id":3002,"type":"lantern"},{"id":20,"type":"parrot"},{"id":21,"type":"mandrill"},{"id":22,"type":"mosquito"},{"id":23,"type":"miss_chief"},{"id":24,"type":"venus"},{"id":25,"type":"jeepers"},{"id":26,"type":"banyan"},{"id":27,"type":"star_girl"},{"id":28,"type":"cosmic_cody"},{"id":29,"type":"chillanna"},{"id":30,"type":"robo"},{"id":31,"type":"raccoon"},{"id":32,"type":"bird"},{"id":58,"type":"toucan"},{"id":59,"type":"anteater"},{"id":3001,"type":"crocodile"},{"id":61,"type":"sloth"},{"id":62,"type":"iguana"},{"id":63,"type":"hut"},{"id":64,"type":"penguin"},{"id":65,"type":"queen_ana"},{"id":66,"type":"yeti"},{"id":67,"type":"deer"},{"id":68,"type":"elf"},{"id":69,"type":"snow_globe"},{"id":70,"type":"polar_bear"},{"id":71,"type":"sleigh"},{"id":72,"type":"mistletoe"},{"id":73,"type":"gonzalo"},{"id":74,"type":"snowflake"},{"id":153,"type":"heart"},{"id":155,"type":"arch"},{"id":156,"type":"squiggle"},{"id":154,"type":"star"},{"id":163,"type":"parallelogram"},{"id":112,"type":"donut"},{"id":160,"type":"fan"},{"id":102,"type":"circle"},{"id":101,"type":"square"},{"id":150,"type":"hexagon"},{"id":151,"type":"triangle"},{"id":105,"type":"right_triangle"},{"id":152,"type":"rectangle"},{"id":157,"type":"z"},{"id":158,"type":"t"},{"id":159,"type":"l"},{"id":116,"type":"corner"},{"id":117,"type":"flower"},{"id":119,"type":"squished_box"},{"id":162,"type":"bead"},{"id":164,"type":"chevron"},{"id":125,"type":"x"},{"id":126,"type":"tetris_line"},{"id":120,"type":"rounded_right_triangle"},{"id":161,"type":"rounded_arrow"},{"id":100,"type":"rounded_square"},{"id":110,"type":"tilted_rectangle"}]
 function convertData(a){
 	result=""
     indent=""
     result+="# Custom Rules\n"
     a.customRules.forEach((e)=>{
-        result+=`rule "${e.name}" (`
+        result+=`rule "${e.name}"=(`
         result+=getCustomParams(e),result+=") {\n"
         result+=getRules(e,indent,a)
         result+="}\n"
@@ -37,7 +37,7 @@ function convertData(a){
     indent=""
     result+="# Abilities\n"
     customBlocks.forEach((e)=>{
-        result+=`ability "${e.name}" (`
+        result+=`ability "${e.name}"=(`
         if(e.hasOwnProperty("parameters"))result+=getCustomParams(e)
         result+=") {\n"
         if(e.hasOwnProperty("blocks"))result+=getBlocks(e,a,indent)
@@ -57,7 +57,7 @@ function getCustomParams(e){
     e.parameters.forEach((f,i)=>{
         a+=`"${convertToString(f.key)}"`
         if(f.value){
-            a+="=",m=f.value.match(regex)
+            a+=":",m=f.value.match(regex)
             if(m)a+=f.value
             else a+=`"${convertToString(f.value)}"`
         }
@@ -74,8 +74,8 @@ function getObjectsFrom(obj,data,indent){
 }
 function getObject(id,data,indent){
     var obj1=data.objects.find((g)=>g.objectID==id)
-    objCode=`object "${convertToString(obj1.name)}" ("image":"${obj1.filename}",${((obj1.filename=="text-object.png")?(`"text":"${obj1.text}",`):"")}"x":${obj1.xPosition},"y":${obj1.yPosition}`
-	if(obj1.hasOwnProperty("rotation"))objCode+=`,"rotation":${obj1.rotation}`
+    objCode=`object "${convertToString(obj1.name)}"=(type:"${obj1.filename}",${((obj1.filename=="text-object.png")?(`text:"${obj1.text}",`):"")}x:${obj1.xPosition},y:${obj1.yPosition}`
+	if(obj1.hasOwnProperty("rotation"))objCode+=`,angle:${obj1.rotation}`
 	objCode+=") {\n"
     var b=data.abilities.find((a)=>a.abilityID==obj1.abilityID)
     if(b)objCode+=getBlocks(b,data,indent)
@@ -92,7 +92,7 @@ function getRules(e,indent,a){
             whens+=indent+`rule "${convertToString(customRule.name)}" (${getParameters(CRI,a)})\n`
         } else {
             var rule=a.rules.find((r)=>r.id==h)
-            whens+=indent+`when(${getEvent(rule,rule.parameters[0],a)}) {\n${findAbility(rule.abilityID,a,indent)+indent}}\n`
+            whens+=indent+`when ${getEvent(rule,rule.parameters[0],a)} {\n${findAbility(rule.abilityID,a,indent)+indent}}\n`
         }
     });
     return whens
@@ -138,7 +138,7 @@ function getBlocks(b,data,indent){
         if(c.type==69)m+=indent+`# ${convertNewLines(c.parameters[0].value)}\n`
         else if(c.type==123)m+=indent+`ability "${convertToString(c.description)}" (${((c.hasOwnProperty("parameters"))?getParameters(c,data):"")})\n`
         else if (c.block_class=="control"){
-            if(c.type==26)m+=indent+`drawTrail(${getParameters(c,data)}) {\n`
+            if(c.type==26)m+=indent+`draw_trail(${getParameters(c,data)}) {\n`
             else if(c.type==120)m+=indent+`repeat(${getParameters(c,data)}) {\n`
             else if(c.type==121)m+=indent+"forever {\n"
             else m+=indent+"<container>{\n"
@@ -190,7 +190,7 @@ function getParamType(a,e,data,i,o=""){
     var params=""
 	if(e.hasOwnProperty("datum")){
         if(e.datum.hasOwnProperty("block_class"))params=getOperators(e.datum,o,data)
-        else if(e.datum.type==8009)params+=`local("${convertToString(e.datum.name)}")`
+        else if(e.datum.type==8009)params+=`local."${convertToString(e.datum.name)}"`
         else if(e.datum.hasOwnProperty("variable")){
             var v=data.variables.find((v)=>v.objectIdString==e.datum.variable)
             params+=getVar(v,e,data)
@@ -205,7 +205,7 @@ function getParamType(a,e,data,i,o=""){
         else {
             var char=chars.find((c)=>c.id==e.datum.type)
             if(char)params+=`"${char.type}"`
-            else params+="<unknown character>"
+            else params+=`<unknown character, id:${e.datum.type}>`
         }
     } else {
         m=e.value.match(regex)
@@ -258,7 +258,7 @@ function getMathParams(a,o,data){
 	    o+=e.key.replace("×","*").replace("÷","/").replace("data",", data ")
         if(e.hasOwnProperty("datum")){
             if(e.datum.hasOwnProperty("block_class"))o=getOperators(e.datum,o,data)
-            else if(e.datum.type==8009)o+=`local("${convertToString(e.datum.name)}")`
+            else if(e.datum.type==8009)o+=`local."${convertToString(e.datum.name)}"`
             else if(e.datum.hasOwnProperty("type")){
                 var v=data.variables.find((v)=>v.objectIdString==e.datum.variable)
                 o+=getVar(v,e,data)
@@ -280,7 +280,7 @@ function getConditionalParams(a,o,data){
         o+=e.key.replace("=","==").replace("≠","!=").replace("matches"," matches ").replace("and"," and ").replace("or"," or ")
 		if(e.hasOwnProperty("datum")){
             if(e.datum.hasOwnProperty("block_class"))o=getOperators(e.datum,o,data)
-            else if(e.datum.type==8009)o+=`local("${convertToString(e.datum.name)}")`
+            else if(e.datum.type==8009)o+=`local."${convertToString(e.datum.name)}"`
             else if(e.datum.hasOwnProperty("type")){
                 var v=data.variables.find((v)=>v.objectIdString==e.datum.variable)
                 o+=getVar(v,e,data)
@@ -298,11 +298,11 @@ function getConditionalParams(a,o,data){
 }
 function getVar(v,e,d){
     if(v.type==8000||v.type==8004||v.type==8005){
-        return `object(${getObjectVarParamType(e,d)}).var("${convertToString(v.name)}")`
+        return `object(${getObjectVarParamType(e,d)}).var."${convertToString(v.name)}"`
         }
-    else if(v.type==8003)return `game.var("${convertToString(v.name)}")`
-    else if(v.type==8007)return `user.var("${convertToString(v.name)}")`
-    else return `product("${convertToString(v.name)}")`
+    else if(v.type==8003)return `game.var."${convertToString(v.name)}"`
+    else if(v.type==8007)return `user.var."${convertToString(v.name)}"`
+    else return `product."${convertToString(v.name)}"`
 }
 function getTraitParamType(e,d){
     if(e.datum.HSTraitObjectParameterTypeKey==8000&&e.datum.hasOwnProperty("HSTraitObjectIDKey"))return `"${getTraitObject(e.datum.HSTraitObjectIDKey,d)}"`
