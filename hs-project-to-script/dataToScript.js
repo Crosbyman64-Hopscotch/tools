21d97cc6d868c321be49d17b1a83ecfa932efe6c
Crosbyman64 <crosbyman216@gmail.com>
2025-12-20 02:22:49 -0500

    Update dataToScript.js

--- hs-project-to-script/dataToScript.js
+++ hs-project-to-script/dataToScript.js
@@ -109,7 +109,7 @@ function getEvent(r,ruleParam,data){
                 }
                 else if(event.id!=7023&&event.id!=7024){
                     eventParams=getEventParams(ruleParam.datum,data)
-                    return event.type+`(${eventParams[0]})`
+                    return `${event.type}.${eventParams[0]}`
                 }
                 else {
                     return event.type+`(${getParamType(r,ruleParam.datum.params[0],data,0)})`
@@ -320,7 +320,7 @@ function getColor(a){
     return z
 }
 
-const getTrait=(t,e,d)=>((t>=2000&&t<=2018)?`object(${getTraitParamType(e,d)})`:((t>=2500&&t<=2507)?"user":"game"))+"."+e.datum.description.replace(/\sas\sa\s%$/,"").replace(/([A-Za-z]+)\s([A-Za-z])/g,(match,group1,group2)=>`${group1.toLowerCase()}_${group2.toLowerCase()}`)
+const getTrait=(t,e,d)=>((t>=2000&&t<=2018)?`object(${getTraitParamType(e,d)})`:((t>=2500&&t<=2507)?"user":"game"))+"."+e.datum.description.replace(/\sas\sa\s%$/,"").replace(/([A-Za-z]+)\s([A-Za-z])/g,(match,group1,group2)=>`${group1.toLowerCase()}_${group2.toLowerCase()}`).replace(/^([A-Z])/,(match,group1)=>group1.toLowerCase())
 const getObjectVarParamType=(e,d)=>((e.datum.type==8000)?`"${getTraitObject(e.datum.object,d)}"`:getTraitType(e.datum.type))
 const getTraitObject=(id,data)=>data.objects.find((a)=>a.objectID==id).name
 const convertToString=(str)=>str.replace(/(\\)(?!n)/g,"\\\\").replace(/\n/g,"\\n").replace(/"/g,'\\"')
