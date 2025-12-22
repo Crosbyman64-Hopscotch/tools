5f16c475d3f055b7df13301077887d398bd59e37
Crosbyman64 <crosbyman216@gmail.com>
2025-12-21 23:52:28 -0500

    Update dataToScript.js

--- hs-project-to-script/dataToScript.js
+++ hs-project-to-script/dataToScript.js
@@ -11,7 +11,7 @@ async function fetchData(url) {
 
     // Await the parsing of the response body (e.g., as JSON)
     const d = await response.json();
-	metadata=d
+	metadata= await response.json();
     return d;
 
   } catch (error) {
@@ -33,7 +33,7 @@ fs.addEventListener("change",function(){
         })
         console.time("time to convert")
 		document.getElementById("upload-text").innerText="Fetching Project Metadata..."
-		const metadata={}
+		var metadata
 		fetchData(`https://corsproxy.io/?url=https://c.gethopscotch.com/api/v1/projects/${data.uuid}`)
 		console.log(metadata)
 	    convertData(data);
